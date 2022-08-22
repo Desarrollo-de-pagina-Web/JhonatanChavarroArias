41a2775312b2d618d202c767563989333d0fab53
JhonatanChavarroArias <jchavarro4@misena.edu.co>
2022-08-22 10:02:06 -0500

    Primer Commit: Jhonatan Chavarro Arias

---
+++ src/main/java/com/example/Jhonatan/Chavarro/Arias/JhonatanChavarroAriasApplication.java
@@ -0,0 +1,17 @@
+package com.example.Jhonatan.Chavarro.Arias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhonatanChavarroAriasApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JhonatanChavarroAriasApplication.class, args);
+		System.out.println("Primer Commit :\nJhonatan Chavarro Arias\n");
+	}
+
+
+}
+
---
+++ src/test/java/com/example/Jhonatan/Chavarro/Arias/JhonatanChavarroAriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jhonatan.Chavarro.Arias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhonatanChavarroAriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
